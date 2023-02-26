9a44e71cd3591ea17cf19f2f8691a2bda7eae45a
amankashyap004 <amankashyap004@gmail.com>
2023-02-26 21:04:19 +0530

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 export default function App() {
 
   return (
-   <div><h2>AMAN'S PORTFOLIO</h2></div>
+   <div className='p-4 flex items-center justify-center'>
+    <h2 className='text-blue-500'>AMAN'S PORTFOLIO</h2>
+   </div>
   )
 }
