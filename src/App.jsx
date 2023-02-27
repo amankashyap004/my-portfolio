09c1c1f67e2185594c3946d07ac34642b0724140
amankashyap004 <amankashyap004@gmail.com>
2023-02-27 17:50:14 +0530

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HeaderPage from "./pages/HeaderPage";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
    return (
       <div>
          <HeaderPage />
+         <HomePage />
       </div>
    );
 }
