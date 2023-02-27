58a679990ca0ead8ef14f8f3a5a5b0374545a413
amankashyap004 <amankashyap004@gmail.com>
2023-02-27 17:56:48 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Home() {
    return (
-      <section>
-         <h1 className="text-red-500">Home</h1>
+      <section className="flex justify-center items-center p-4">
+         <section className="mr-4 text-white">First Section</section>
+         <section className="text-white">Second Section</section>
       </section>
    );
 }
