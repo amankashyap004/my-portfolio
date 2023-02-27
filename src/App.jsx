703164787a51b08c1493a9e11cadb0767dfa1e9b
amankashyap004 <amankashyap004@gmail.com>
2023-02-27 13:00:22 +0530

    cearet header page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Header from "./pages/Header";
 
 export default function App() {
-
-  return (
-   <div className='p-4 flex items-center justify-center'>
-    <h2 className='text-blue-500'>AMAN'S PORTFOLIO</h2>
-   </div>
-  )
+   return (
+      <div>
+         <Header />
+      </div>
+   );
 }
