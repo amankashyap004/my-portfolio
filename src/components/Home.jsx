57c17fb3c7b7955c0e462f1b8f68e33a85c07ee9
amankashyap004 <amankashyap004@gmail.com>
2023-02-28 18:15:22 +0530

    create small btn

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import SmallBtn from "./SmallBtn";
 
 export default function Home() {
    return (
-      <section className="flex justify-center items-center p-4 w-full h-[60vh]">
+      <section className="flex justify-center items-center p-4 w-full h-[70vh]">
          <section className="mr-4 text-white w-[40%] p-8">
             <h1 className="font-bold text-2xl">
                Hello, my name is <span className="text-blue-500">Aman Kashyap</span>
@@ -15,6 +16,10 @@ export default function Home() {
                contact me. Take a look around and let me know if you have any questions or would
                like to work together!
             </p>
+            <div className="mt-8 flex justify-start items-center">
+               <SmallBtn value="Download CV" />
+               <SmallBtn value="Hire Me!" />
+            </div>
          </section>
          <section className="text-white w-[40%]">Second Section</section>
       </section>
