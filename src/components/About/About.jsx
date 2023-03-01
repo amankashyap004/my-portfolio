c8cdecb942b540764e888474ddf3772943f98c92
amankashyap004 <amankashyap004@gmail.com>
2023-03-01 18:04:31 +0530

    create introduction section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Introduction from "./Introduction";
 
 export default function About() {
    return (
       <section className="flex justify-center items-start flex-col m-4">
          {/* profile */}
          {/* introduction */}
+         <Introduction />
          {/* personal information */}
          {/* education */}
          {/* skills */}
---
+++ src/components/About/Introduction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Introduction() {
+   return (
+      <div className="flex justify-center items-center">
+         <p className="text-white w-1/2">
+            Hi there! My name is Aman Kashyap, and I am a passionate front-end web developer. I
+            specialize in building user-friendly and responsive interfaces using HTML, CSS,
+            JavaScript, and React. I have honed my skills in creating visually stunning websites
+            that provide an excellent user experience. My goal as a developer is to create engaging
+            and interactive digital experiences that captivate audiences and make a positive impact
+            on their lives. I'm excited to continue my journey in web development and explore new
+            possibilities in this dynamic and ever-evolving field.
+         </p>
+      </div>
+   );
+}
