943bccb806c782b1bec0dba3818c44a15014acb7
amankashyap004 <amankashyap004@gmail.com>
2023-03-01 17:25:03 +0530

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import HeaderPage from "./pages/HeaderPage";
 import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
    return (
-      <div>
+      <BrowserRouter>
          <HeaderPage />
-         <HomePage />
-      </div>
+         <Routes>
+            <Route path={`/`} element={<HomePage />} />
+         </Routes>
+      </BrowserRouter>
    );
 }
