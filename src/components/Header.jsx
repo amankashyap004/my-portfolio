40940cd6dc457e7e94d139af862a0e377c31e364
amankashyap004 <amankashyap004@gmail.com>
2023-03-01 18:49:07 +0530

    edit logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,21 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
    return (
-      <header className="bg-slate-700 px-6 py-4 text-white flex justify-between items-center md:p-4">
-         <div className="font-bold text-2xl md:text-xl">
-            <span>AMAN</span>
-            <span className="text-red-500">'</span>
-            <span>S </span>
-            <span>PORTFOLIO</span>
-         </div>
+      <header className="bg-slate-700 px-6 py-2 text-white flex justify-between items-center md:px-4">
+         <Link to="/">
+            <div className="cursor-pointer">
+               <h2 className="font-bold text-2xl md:text-xl ">
+                  <span className="text-blue-500">A</span>
+                  <span>MAN</span>
+                  <span className="text-blue-500">'</span>
+                  <span>S </span>
+               </h2>
+               <h4 className="flex justify-end items-center text-sm font-semibold ml-8">
+                  <span>PORT</span>
+                  <span className=" text-blue-500">FOLIO</span>
+               </h4>
+            </div>
+         </Link>
          <div className="flex justify-between items-center">
             <Link to="/">
                <NavigationContent textValue="Home" />
