9cf6c0c2ac53ef101f55a24beaea879ddf54c4b1
amankashyap004 <amankashyap004@gmail.com>
2023-03-01 17:57:58 +0530

    create about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+   return (
+      <section className="flex justify-center items-start flex-col m-4">
+         {/* profile */}
+         {/* introduction */}
+         {/* personal information */}
+         {/* education */}
+         {/* skills */}
+      </section>
+   );
+}
