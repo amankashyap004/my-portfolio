6a366a7d176f38a6758f835b9b4888dc52af25cb
amankashyap004 <amankashyap004@gmail.com>
2023-03-01 17:34:14 +0530

    create about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderPage from "./pages/HeaderPage";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
    return (
@@ -10,6 +11,7 @@ export default function App() {
          <HeaderPage />
          <Routes>
             <Route path={`/`} element={<HomePage />} />
+            <Route path={`/about`} element={<AboutPage />} />
          </Routes>
       </BrowserRouter>
    );
