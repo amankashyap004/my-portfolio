1422b225a02ef2e53529e0d9211f368f33952ada
amankashyap004 <amankashyap004@gmail.com>
2023-03-02 19:56:46 +0530

    Social Media

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { FaLinkedinIn, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import SocialMediaContainer from "./SocialMediaContainer";
 
 export default function SocialMedia() {
-   return <section className="flex justify-center items-center m-4"></section>;
+   return (
+      <section className="flex justify-center items-center m-4">
+         <SocialMediaContainer icon={<FaGithub />} />
+         <SocialMediaContainer icon={<FaLinkedinIn />} />
+         <SocialMediaContainer icon={<FaInstagram />} />
+         <SocialMediaContainer icon={<FaTwitter />} />
+      </section>
+   );
 }
