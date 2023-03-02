e689336808631093c3831f1d3b92a8965f7900ae
amankashyap004 <amankashyap004@gmail.com>
2023-03-02 20:47:28 +0530

    Social Media Container

--- src/components/SocialMediaContainer.jsx
+++ src/components/SocialMediaContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SocialMediaContainer({ icon }) {
    return (
-      <div className=" text-base rounded-full p-1.5 bg-blue-500 cursor-pointer hover:text-blue-800 hover:bg-blue-300 mr-2">
+      <div className="text-base rounded-full p-1.5 text-black bg-blue-500 cursor-pointer hover:text-blue-800 hover:bg-blue-300 mr-2">
          {icon}
       </div>
    );
