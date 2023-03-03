e4eaff7a49e5b90e8f9178bfbbddbcef448f6267
amankashyap004 <amankashyap004@gmail.com>
2023-03-03 19:16:47 +0530

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Contact() {
-   return <section>Contact</section>;
+   return (
+      <section className="flex justify-center items-start m-4">
+         <div className="px-4 py-8 flex justify-center items-center flex-col">
+            <section className="flex justify-center items-center flex-col my-2">
+               <h2 className="text-white font-bold text-3xl mb-2">Contact</h2>
+               <p className="text-gray-500 text-sm">
+                  We'd love to hear from you! Send us a message and let's make something amazing
+                  together.
+               </p>
+            </section>
+         </div>
+      </section>
+   );
 }
