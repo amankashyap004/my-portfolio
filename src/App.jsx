f7579707e582d5333b09c8b4b94ecb26fd2b702c
amankashyap004 <amankashyap004@gmail.com>
2023-03-03 18:13:56 +0530

    link contact page on app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderPage from "./pages/HeaderPage";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 export default function App() {
    return (
@@ -12,6 +13,7 @@ export default function App() {
          <Routes>
             <Route path={`/`} element={<HomePage />} />
             <Route path={`/about`} element={<AboutPage />} />
+            <Route path={`/contact`} element={<ContactPage />} />
          </Routes>
       </BrowserRouter>
    );
