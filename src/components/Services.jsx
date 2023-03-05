646a53bbbf3854ddad7735ace5dbc7e3f91b6b47
amankashyap004 <amankashyap004@gmail.com>
2023-03-05 19:29:44 +0530

    services heading

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Services() {
    return (
-      <section>
-         <div>
-            <h2>Our Services</h2>
+      <section className="flex justify-center items-center m-4">
+         <div className="m-4">
+            <h2 className="text-white font-bold text-2xl">Our Services</h2>
          </div>
       </section>
    );
