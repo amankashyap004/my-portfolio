f4530053978d1061c53b68270cfd1ecdafbdc58a
amankashyap004 <amankashyap004@gmail.com>
2023-03-05 20:56:10 +0530

    add some css on services card

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,8 +5,8 @@ import ServicesCard from "./ServicesCard";
 export default function Services() {
    return (
       <section className="flex justify-center items-center flex-col m-4">
-         <div className="m-4">
-            <h2 className="text-white font-bold text-2xl">Our Services</h2>
+         <div className="mt-8">
+            <h2 className="text-white font-bold text-2xl uppercase">Our Services</h2>
          </div>
          <div className="w-full p-4 flex justify-center items-center">
             <ServicesCard />
--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -3,12 +3,14 @@ import { webImg } from "../assets/images";
 
 export default function ServicesCard() {
    return (
-      <section className="bg-gray-700 p-4 rounded-lg flex justify-center items-center flex-col w-1/4 m-4">
-         <div className="w-[250px]">
+      <section className="bg-gray-800 hover:bg-gray-700 ease-in-out delay-150 p-4 rounded-lg flex justify-center items-center flex-col w-1/4 m-4 cursor-pointer">
+         <div className="w-[300px] p-2">
             <img src={webImg} className="w-full" />
          </div>
-         <div className="py-4">
-            <h3 className="text-white font-semibold text-xl text-center pt-2">Web Development</h3>
+         <div className="p-1">
+            <h3 className="text-white font-semibold text-xl text-center pt-2 uppercase">
+               Web Development
+            </h3>
             <div className="w-full h-[2px] bg-blue-700 my-2 shadow-md shadow-blue-200"></div>
             <p className="text-gray-400 text-justify">
                "Hey there, As a web developer I specialize in creating beautiful and functional
