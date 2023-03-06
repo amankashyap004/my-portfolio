8dc67236c270ab0800ed371366a2de63be721c69
amankashyap004 <amankashyap004@gmail.com>
2023-03-06 20:28:49 +0530

    create project card

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProjectCard() {
+   return (
+      <section className="bg-gray-800 hover:bg-gray-700 ease-in-out delay-150 p-4 rounded-lg flex justify-center items-center flex-col w-1/3 m-4 cursor-pointer"></section>
+   );
+}
