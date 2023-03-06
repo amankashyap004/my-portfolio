6774a9764c439dd16eaa8a6f264607e64324a5a3
amankashyap004 <amankashyap004@gmail.com>
2023-03-06 19:56:28 +0530

    create project page

---
+++ src/components/Project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Project() {
+   return (
+      <section className="flex justify-center items-start m-4">
+         <div className="mt-7 sm:mt-4">
+            <h2 className="text-white font-bold text-2xl uppercase sm:text-xl">My Works</h2>
+         </div>
+      </section>
+   );
+}
---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Project from "../components/Project";
+
+export default function ProjectPage() {
+   return (
+      <div className="mt-16">
+         <Project />
+      </div>
+   );
+}
