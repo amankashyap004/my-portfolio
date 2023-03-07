a24ef5f1a399ae3b6770ad565997fe8ff3925300
amankashyap004 <amankashyap004@gmail.com>
2023-03-07 19:05:30 +0530

    add task-hive link on project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -18,26 +18,8 @@ export default function Project() {
             up and log in to your account. You can access all your previous tasks and check them
             off as completed. Task Hive is built using the latest web technologies like Tailwind
             CSS, React JS, and Firebase, ensuring a fast and responsive experience."
-            />
-            <ProjectCard
-               image={taskHiveImg}
-               heading="Task Hive"
-               description="Task Hive is a powerful task management tool that helps you stay on top of your to-do
-            list. With its user-friendly interface, you can easily create and organize your
-            tasks, and prioritize them based on their importance. To use Task Hive, simply sign
-            up and log in to your account. You can access all your previous tasks and check them
-            off as completed. Task Hive is built using the latest web technologies like Tailwind
-            CSS, React JS, and Firebase, ensuring a fast and responsive experience."
-            />
-            <ProjectCard
-               image={taskHiveImg}
-               heading="Task Hive"
-               description="Task Hive is a powerful task management tool that helps you stay on top of your to-do
-            list. With its user-friendly interface, you can easily create and organize your
-            tasks, and prioritize them based on their importance. To use Task Hive, simply sign
-            up and log in to your account. You can access all your previous tasks and check them
-            off as completed. Task Hive is built using the latest web technologies like Tailwind
-            CSS, React JS, and Firebase, ensuring a fast and responsive experience."
+               codeLink="https://github.com/amankashyap004/task-hive"
+               liveLink="https://amankashyap004.github.io/task-hive"
             />
          </div>
       </section>
