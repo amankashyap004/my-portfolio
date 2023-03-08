946ea14f07ad2a32a6b104cbf22d9f5ed91f1258
amankashyap004 <amankashyap004@gmail.com>
2023-03-08 19:27:02 +0530

    card game page

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -3,5 +3,6 @@ import webImg from "./Image1.png";
 import taskHiveImg from "./task-hive.png";
 import postGeneratorImg from "./post-generator.png";
 import reactYoutubeCloneImg from "./react-youtube-clone.png";
+import demoImg from "./demo-image.jpg";
 
-export { profileImage, webImg, taskHiveImg, postGeneratorImg, reactYoutubeCloneImg };
+export { profileImage, webImg, taskHiveImg, postGeneratorImg, reactYoutubeCloneImg, demoImg };
