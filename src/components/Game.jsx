334f9f5ebf71db5f6b9c9176ed7deafb75df0417
amankashyap004 <amankashyap004@gmail.com>
2023-03-08 20:12:55 +0530

    fix game page

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GameCard from "./GameCard";
+import { demoImg } from "../assets/images";
 
 export default function Game() {
    return (
@@ -7,11 +8,27 @@ export default function Game() {
          <div className="mt-7">
             <h2 className="text-white font-bold text-2xl uppercase">Game</h2>
          </div>
-         <div className="w-full flex justify-center items-start flex-wrap mt-4">
-            <GameCard />
-            <GameCard />
-            <GameCard />
-            <GameCard />
+         <div className="w-full flex justify-center items-start mt-4">
+            <GameCard
+               bgImg={demoImg}
+               heading="Game Title"
+               description="A game is an activity or sport usually involving skill, knowledge, or chance"
+            />
+            <GameCard
+               bgImg={demoImg}
+               heading="Game Title"
+               description="A game is an activity or sport usually involving skill, knowledge, or chance"
+            />
+            <GameCard
+               bgImg={demoImg}
+               heading="Game Title"
+               description="A game is an activity or sport usually involving skill, knowledge, or chance"
+            />
+            <GameCard
+               bgImg={demoImg}
+               heading="Game Title"
+               description="A game is an activity or sport usually involving skill, knowledge, or chance"
+            />
          </div>
       </section>
    );
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { demoImg } from "../assets/images";
 
-export default function GameCard() {
+export default function GameCard({ bgImg, heading, description }) {
    return (
-      <section className="ease-in-out delay-150 rounded-lg w-[330px] m-4 cursor-pointer relative">
-         <img src={demoImg} className="w-full rounded-lg opacity-75 hover:opacity-50" />
+      <section className="ease-in-out delay-150 rounded-lg w-1/4 m-4 cursor-pointer relative">
+         <img src={bgImg} className="w-full rounded-lg opacity-75 hover:opacity-50" />
          <h3 className="absolute top-0 text-white text-base uppercase pt-2 pl-2 font-black z-10">
-            New game
+            {heading}
          </h3>
-         <p className="absolute bottom-0 text-white font-extrabold p-2 z-10">
-            A game is an activity or sport usually involving skill, knowledge, or chance, ...
+         <p className="absolute bottom-0 text-white text-sm font-extrabold p-2 z-10">
+            {description}&nbsp;...
          </p>
       </section>
    );
