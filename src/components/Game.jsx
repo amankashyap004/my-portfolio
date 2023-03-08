688cba1d5064f227fd7bd46aa03d79376026bee5
amankashyap004 <amankashyap004@gmail.com>
2023-03-08 20:41:34 +0530

    link feature on game card

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -18,21 +18,25 @@ export default function Game() {
                bgImg={snakeGunWaterImg}
                heading="Snake Gun Water"
                description="Snake Gun Water Game is one of the famous two-player game played by many people."
+               link="https://amankashyap004.github.io/snake-gun-water-game/"
             />
             <GameCard
                bgImg={guessesTheNumberImg}
                heading="Guess The Number"
                description="Guess The Number is a fun game that challenges to guess a number."
+               link="https://amankashyap004.github.io/guess-the-number/"
             />
             <GameCard
                bgImg={footballScoreboardImg}
                heading="Football Scoreboard"
                description="It is a simple Football Scoreboard website"
+               link="https://amankashyap004.github.io/football-scoreboard/"
             />
             <GameCard
                bgImg={randomMotivationalQuotesImg}
                heading="Random Motivational Quotes"
                description="Random Motivational Quotes website is awesome random quotes generator"
+               link="https://amankashyap004.github.io/random-motivational-quotes/"
             />
          </div>
       </section>
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function GameCard({ bgImg, heading, description }) {
+export default function GameCard({ bgImg, heading, description, link }) {
    return (
-      <section className="ease-in-out delay-150 rounded-lg w-1/4 m-4 cursor-pointer relative">
-         <img src={bgImg} className="w-full rounded-lg opacity-75 hover:opacity-50" />
-         <h3 className="absolute top-0 text-white text-base uppercase pt-2 pl-2 font-black z-10">
-            {heading}
-         </h3>
-         <p className="absolute bottom-0 text-white text-sm font-extrabold p-2 z-10">
-            {description}&nbsp;...
-         </p>
-      </section>
+      <Link to={link ? link : "/"}>
+         <section className="ease-in-out delay-150 rounded-lg w-[330px] m-4 cursor-pointer relative">
+            <img src={bgImg} className="w-full rounded-lg opacity-75 hover:opacity-50" />
+            <h3 className="absolute top-0 text-white text-base uppercase pt-2 pl-2 font-black z-10">
+               {heading}
+            </h3>
+            <p className="absolute bottom-0 text-white text-sm font-extrabold p-2 z-10">
+               {description}&nbsp;...
+            </p>
+         </section>
+      </Link>
    );
 }
