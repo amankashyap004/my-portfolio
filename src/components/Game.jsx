7d4ac579ff61c2f6674fb609eb784d89905f9f83
amankashyap004 <amankashyap004@gmail.com>
2023-03-08 18:47:46 +0530

    create game page

---
+++ src/components/Game.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Game() {
+   return (
+      <section className="flex justify-center items-center flex-col m-4">
+         <div className="mt-7">
+            <h2 className="text-white font-bold text-2xl uppercase">Game</h2>
+         </div>
+      </section>
+   );
+}
---
+++ src/pages/GamePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Game from "../components/Game";
+
+export default function GamePage() {
+   return (
+      <div className="mt-16">
+         <Game />
+      </div>
+   );
+}
