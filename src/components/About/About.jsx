cdbedcbcbeed613a6ca7e73dd417f7d8993d61ec
amankashyap004 <amankashyap004@gmail.com>
2023-03-09 20:29:20 +0530

    fixed some feature on about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,20 @@ import Profile from "./Profile";
 
 export default function About() {
    return (
-      <section className="flex justify-center items-start m-4">
-         <div className="flex justify-center items-center flex-col w-[20%] p-4">
-            <Profile />
-         </div>
-         <div className="h-[100vh] w-[1.5px] bg-gray-500"></div>
-         <div className="flex justify-between items-center w-[80%]">
-            <Introduction />
-         </div>
-         {/* personal information */}
-         {/* education */}
-         {/* skills */}
+      <section className="flex justify-center items-start m-4 relative">
+         <aside className="flex justify-center items-start w-[18%] h-full fixed top-16 left-4 border-r-2 border-slate-700">
+            <div className="mt-6">
+               <Profile />
+            </div>
+         </aside>
+         <section className="w-[82%] absolute left-[18%]">
+            <div className="flex justify-between items-center flex-col">
+               <Introduction />
+            </div>
+            {/* personal information */}
+            {/* education */}
+            {/* skills */}
+         </section>
       </section>
    );
 }
