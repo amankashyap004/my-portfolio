e4869a9e19904ce1377888b9192307036ed0b74e
amankashyap004 <amankashyap004@gmail.com>
2023-03-09 21:08:27 +0530

    add menu icon on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderNav from "./HeaderNav";
 import { Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
 
 export default function Header() {
    return (
@@ -19,8 +20,11 @@ export default function Header() {
                </h4>
             </div>
          </Link>
-         <div>
+         <div className="sm:hidden">
             <HeaderNav />
+         </div>
+         <div className="hidden sm:block">
+            <BiMenu className="text-2xl" />
          </div>
       </header>
    );
