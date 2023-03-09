cd12a61d8494f2b2ff91e36279c75664a0425016
amankashyap004 <amankashyap004@gmail.com>
2023-03-09 21:53:39 +0530

    css in NavigationContent

--- src/components/NavigationContent.jsx
+++ src/components/NavigationContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavigationContent({ textValue }) {
    return (
-      <div className="mx-2 cursor-pointer">
+      <div className="mx-2 cursor-pointer sm:m-1">
          <samp className="font-semibold text-lg uppercase text-gray-100 hover:text-blue-200 md:text-base">
             {textValue}
          </samp>
