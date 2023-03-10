b3ca615e39a14f71552bdc34845d17bf3be559b6
amankashyap004 <amankashyap004@gmail.com>
2023-03-10 20:40:00 +0530

    create Personal Info Card

--- src/components/About/PersonalInfo.jsx
+++ src/components/About/PersonalInfo.jsx
@@ -1,18 +1,11 @@
 import React from "react";
+import PersonalInfoCard from "./PersonalInfoCard";
 import { FaUser } from "react-icons/fa";
 
 export default function PersonalInfo() {
    return (
       <section className="grid grid-cols-3 gap-4 p-4">
-         <section className="flex justify-center items-start px-4 py-3 my-2 cursor-pointer bg-blue-800 rounded-lg hover:bg-blue-700 transition-all duration-500 ease-in-out ">
-            <div className="flex justify-start items-start flex-col">
-               <p className="text-gray-400 font-extrabold text-base uppercase mb-2">Full Name</p>
-               <p className="text-gray-200 font-medium uppercase">Aman Kumar Kashyap</p>
-            </div>
-            <div className="text-lg text-blue-500 hover:text-red-400 cursor-pointer ml-4">
-               <FaUser />
-            </div>
-         </section>
+         <PersonalInfoCard title="Full Name" name="Aman Kumar Kashyap" icon={<FaUser />} />
       </section>
    );
 }
---
+++ src/components/About/PersonalInfoCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PersonalInfoCard({ title, name, icon }) {
+   return (
+      <section className="flex justify-center items-start px-4 py-3 my-2 cursor-pointer bg-blue-800 rounded-lg hover:bg-blue-700 transition-all duration-500 ease-in-out ">
+         <div className="flex justify-start items-start flex-col">
+            <p className="text-gray-400 font-extrabold text-base uppercase mb-2">{title}</p>
+            <p className="text-gray-200 font-medium uppercase">{name}</p>
+         </div>
+         <div className="text-lg text-blue-500 hover:text-red-400 cursor-pointer ml-4">{icon}</div>
+      </section>
+   );
+}
