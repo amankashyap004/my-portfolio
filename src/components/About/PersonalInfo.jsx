0b9e6283a94c7f5d6b6921215793c0d131b679d9
amankashyap004 <amankashyap004@gmail.com>
2023-03-10 20:35:07 +0530

    personal info card

--- src/components/About/PersonalInfo.jsx
+++ src/components/About/PersonalInfo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { FaUser } from "react-icons/fa";
 
 export default function PersonalInfo() {
    return (
-      <div className="flex justify-center items-center p-2">
-         <h2> Personal Info</h2>
-      </div>
+      <section className="grid grid-cols-3 gap-4 p-4">
+         <section className="flex justify-center items-start px-4 py-3 my-2 cursor-pointer bg-blue-800 rounded-lg hover:bg-blue-700 transition-all duration-500 ease-in-out ">
+            <div className="flex justify-start items-start flex-col">
+               <p className="text-gray-400 font-extrabold text-base uppercase mb-2">Full Name</p>
+               <p className="text-gray-200 font-medium uppercase">Aman Kumar Kashyap</p>
+            </div>
+            <div className="text-lg text-blue-500 hover:text-red-400 cursor-pointer ml-4">
+               <FaUser />
+            </div>
+         </section>
+      </section>
    );
 }
