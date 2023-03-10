97a2317bb58bf9a5ca596431e7c4259c7f5bbc8e
amankashyap004 <amankashyap004@gmail.com>
2023-03-10 20:44:57 +0530

    add Email on Personal Info

--- src/components/About/PersonalInfo.jsx
+++ src/components/About/PersonalInfo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PersonalInfoCard from "./PersonalInfoCard";
 import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function PersonalInfo() {
    return (
       <section className="grid grid-cols-3 gap-4 p-4">
          <PersonalInfoCard title="Full Name" name="Aman Kumar Kashyap" icon={<FaUser />} />
+         <PersonalInfoCard title="Email" name="amankashyap004@gmail.com" icon={<MdEmail />} />
       </section>
    );
 }
