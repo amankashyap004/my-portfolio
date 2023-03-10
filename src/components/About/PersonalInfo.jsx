64a56defcbda346660ca0e77b320dd17e50c11e5
amankashyap004 <amankashyap004@gmail.com>
2023-03-10 21:06:49 +0530

    add all Personal Info

--- src/components/About/PersonalInfo.jsx
+++ src/components/About/PersonalInfo.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PersonalInfoCard from "./PersonalInfoCard";
-import { FaUser } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { FaUser, FaCalendarAlt } from "react-icons/fa";
+import { MdEmail, MdLanguage } from "react-icons/md";
+import { GiWorld } from "react-icons/gi";
 
 export default function PersonalInfo() {
    return (
-      <section className="grid grid-cols-3 gap-4 p-4">
+      <section className="grid grid-cols-3 gap-4 p-8">
          <PersonalInfoCard title="Full Name" name="Aman Kumar Kashyap" icon={<FaUser />} />
+         <PersonalInfoCard title="Date Of Birth" name="28 july 1997" icon={<FaCalendarAlt />} />
          <PersonalInfoCard title="Email" name="amankashyap004@gmail.com" icon={<MdEmail />} />
+         <PersonalInfoCard title="Address" name="Raipur, India" icon={<GiWorld />} />
+         <PersonalInfoCard title="Language" name="Hindi, English" icon={<MdLanguage />} />
       </section>
    );
 }
