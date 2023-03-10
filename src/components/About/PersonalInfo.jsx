c4660eb92a0f3631de727ba6715d9a1bcab57ea5
amankashyap004 <amankashyap004@gmail.com>
2023-03-10 21:49:44 +0530

    edit about page

--- src/components/About/PersonalInfo.jsx
+++ src/components/About/PersonalInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PersonalInfoCard from "./PersonalInfoCard";
 import { FaUser, FaCalendarAlt } from "react-icons/fa";
-import { MdEmail, MdLanguage } from "react-icons/md";
+import { MdEmail, MdLocationPin } from "react-icons/md";
 import { GiWorld } from "react-icons/gi";
 
 export default function PersonalInfo() {
@@ -10,8 +10,8 @@ export default function PersonalInfo() {
          <PersonalInfoCard title="Full Name" name="Aman Kumar Kashyap" icon={<FaUser />} />
          <PersonalInfoCard title="Date Of Birth" name="28 july 1997" icon={<FaCalendarAlt />} />
          <PersonalInfoCard title="Email" name="amankashyap004@gmail.com" icon={<MdEmail />} />
-         <PersonalInfoCard title="Address" name="Raipur, India" icon={<GiWorld />} />
-         <PersonalInfoCard title="Language" name="Hindi, English" icon={<MdLanguage />} />
+         <PersonalInfoCard title="Address" name="Raipur, India" icon={<MdLocationPin />} />
+         <PersonalInfoCard title="Language" name="Hindi, English" icon={<GiWorld />} />
       </section>
    );
 }
--- src/components/About/PersonalInfoCard.jsx
+++ src/components/About/PersonalInfoCard.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function PersonalInfoCard({ title, name, icon }) {
    return (
-      <section className="flex justify-between items-start px-4 py-3 cursor-pointer bg-blue-800 rounded-lg hover:bg-blue-700 transition-all duration-500 ease-in-out md:px-2 sm:w-full">
+      <section className="flex justify-between items-start px-4 py-3 cursor-pointer bg-blue-800 rounded-lg hover:bg-blue-700 transition-all duration-500 ease-in-out md:px-2 sm:w-full sm:px-4">
          <div className="flex justify-start items-start flex-col">
             <p className="text-gray-400 font-extrabold text-base uppercase mb-2">{title}</p>
             <p className="text-gray-200 font-medium">{name}</p>
          </div>
-         <div className="text-xl text-blue-500 hover:text-red-400 cursor-pointer ml-4">{icon}</div>
+         <div className="text-xl text-blue-500 hover:text-red-400 cursor-pointer ml-4 sm:text-lg">
+            {icon}
+         </div>
       </section>
    );
 }
