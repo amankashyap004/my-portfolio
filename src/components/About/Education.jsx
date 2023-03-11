4b86e00b0d1c136cbdfd9435b29f985c786aeb30
amankashyap004 <amankashyap004@gmail.com>
2023-03-11 18:51:21 +0530

    create Education section

--- src/components/About/Education.jsx
+++ src/components/About/Education.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Education() {
-   return <div className="flex justify-center items-center p-2">Education</div>;
+   return (
+      <section className="w-full flex justify-start items-start flex-col py-4">
+         <section className="w-full p-4 border-2 border-bg-gray-400 rounded-lg">
+            <div className="flex justify-between items-center">
+               <h3 className="font-bold text-lg text-white">
+                  CHHATTISGARH SWAMI VIVEKANAND TECHNICAL UNIVERSITY
+               </h3>
+               <samp className="font-bold text-base text-gray-500 italic">
+                  BHILAI, CHHATTISGARH
+               </samp>
+            </div>
+            <div className="flex justify-between items-center">
+               <h3 className="font-semibold text-base text-gray-400">Bachelor of Engineering in</h3>
+               <samp className="font-bold text-base text-gray-500 italic">Jul 2015 - May 2019</samp>
+            </div>
+            <div className="flex justify-between items-center">
+               <h3 className="font-semibold text-base text-gray-500">
+                  Electronic and Telecommunication
+               </h3>
+            </div>
+         </section>
+      </section>
+   );
 }
