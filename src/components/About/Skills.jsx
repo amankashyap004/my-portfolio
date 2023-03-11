cee98792c506b0c039b995f7a69748519c18a66a
amankashyap004 <amankashyap004@gmail.com>
2023-03-11 13:55:50 +0530

    add some more progress bar

--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -4,6 +4,10 @@ export default function Skills() {
    return (
       <section className="w-1/2 p-4">
          <ProgressBar title="HTML" percentage="85%" />
+         <ProgressBar title="CSS" percentage="70%" />
+         <ProgressBar title="JAVASCRIPT" percentage="65%" />
+         <ProgressBar title="REACT JS" percentage="60%" />
+         <ProgressBar title="TAILWIND CSS" percentage="60%" />
       </section>
    );
 }
