a49be30d30bbb553c33cb4e2f8b4d790966d8e7f
amankashyap004 <amankashyap004@gmail.com>
2023-03-11 13:52:47 +0530

    fix progress bar

--- src/components/About/ProgressBar.jsx
+++ src/components/About/ProgressBar.jsx
@@ -1,12 +1,15 @@
-export default function ProgressBar() {
+export default function ProgressBar({ title, percentage }) {
    return (
-      <div className="w-full p-4">
+      <div className="w-full py-2">
          <div className="flex justify-between mb-1">
-            <span className="text-base font-medium text-white">HTML</span>
-            <span className="text-sm font-medium text-white">80%</span>
+            <span className="text-base font-medium text-white">{title}</span>
+            <span className="text-sm font-medium text-white">{percentage}</span>
          </div>
          <div className="w-full bg-gray-200 rounded-full h-2.5">
-            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: "80%" }}></div>
+            <div
+               className="bg-blue-600 h-2.5 rounded-full"
+               style={{ width: `${percentage}` }}
+            ></div>
          </div>
       </div>
    );
--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -3,7 +3,7 @@ import ProgressBar from "./ProgressBar";
 export default function Skills() {
    return (
       <section className="w-1/2 p-4">
-         <ProgressBar />
+         <ProgressBar title="HTML" percentage="85%" />
       </section>
    );
 }
