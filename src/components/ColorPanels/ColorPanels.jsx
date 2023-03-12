bee255318fbdc413eb4750380ade01b5ee87c77f
amankashyap004 <amankashyap004@gmail.com>
2023-03-12 14:33:47 +0530

    add color feature

--- src/components/ColorPanels/ColorPanels.jsx
+++ src/components/ColorPanels/ColorPanels.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function ColorPanel() {
    return (
-      <section>
-         <h1>Color Panel</h1>
+      <section className="flex justify-center items-center">
+         <div className="rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+         <div className="bg-gray-500 p-4 flex justify-center items-center rounded-bl-md">
+            <div className="rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+            <div className="bg-blue-500 rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+            <div className="bg-red-500 rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+            <div className="bg-green-500 rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+            <div className="bg-orange-500 rounded-full p-4 w-3 h-3 border-2 border-gray-100 cursor-pointer m-1"></div>
+         </div>
       </section>
    );
 }
--- src/pages/ColorPanelsPage.jsx
+++ src/pages/ColorPanelsPage.jsx
@@ -3,7 +3,7 @@ import ColorPanel from "../components/ColorPanels/ColorPanels";
 
 export default function ColorPanelPage() {
    return (
-      <div className="fixed top-16">
+      <div className="fixed top-16 right-0">
          <ColorPanel />
       </div>
    );
