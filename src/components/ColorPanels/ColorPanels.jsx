e92f85d0001c443d49e7df1a1d0bfa62bd9a2a8e
amankashyap004 <amankashyap004@gmail.com>
2023-03-12 14:12:33 +0530

    create ColorPanelPage

---
+++ src/components/ColorPanels/ColorPanels.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ColorPanel() {
+   return (
+      <section>
+         <h1>Color Panel</h1>
+      </section>
+   );
+}
---
+++ src/pages/ColorPanelsPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ColorPanel from "../components/ColorPanels/ColorPanels";
+
+export default function ColorPanelPage() {
+   return (
+      <div className="fixed top-16">
+         <ColorPanel />
+      </div>
+   );
+}
