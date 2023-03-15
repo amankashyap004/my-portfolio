6e47434f5da69ebe84e90996d2e81b8ff2bdea43
amankashyap004 <amankashyap004@gmail.com>
2023-03-15 18:54:27 +0530

    css in Colors Panel

--- src/components/ColorsPanel/ColorsPanel.jsx
+++ src/components/ColorsPanel/ColorsPanel.jsx
@@ -19,14 +19,14 @@ export default function ColorsPanel() {
       <section className="flex justify-center items-center">
          <div
             className={`cursor-pointer mr-2 flex justify-center items-center transition-all
-            ${isColorsPanel ? "" : "translate-x-[800%] sm:translate-x-[830%]"}`}
+            ${isColorsPanel ? "translate-x-0" : "translate-x-[800%] sm:translate-x-[830%]"}`}
             onClick={handelColorsPanel}
          >
             <AiFillSetting className="text-blue-500 hover:text-blue-400 active:text-blue-600 text-3xl z-10 text-center transition-all duration-500 ease-in-out" />
          </div>
          <div
             className={`bg-gray-600 p-4 flex justify-center items-center rounded-bl-md transition-all
-            ${isColorsPanel ? "translate-x-[0%]" : "translate-x-full"}`}
+            ${isColorsPanel ? "translate-x-0" : "translate-x-full"}`}
          >
             <div
                className="flex justify-center items-center rounded-full p-4 w-3 h-3 border-2 cursor-pointer m-1 border-gray-300 hover:border-gray-400 active:shadow-lg transition-all duration-500 ease-in-out"
