b8e5749f3ddcaa8d685bdbedc87506e913ecfd2a
amankashyap004 <amankashyap004@gmail.com>
2023-03-16 18:51:15 +0530

    add dark mode in app

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,38 @@ import ServicesPage from "./pages/ServicesPage";
 import ProjectPage from "./pages/ProjectPage";
 import GamePage from "./pages/GamePage";
 import ColorsPanelPage from "./pages/ColorsPanelPage";
+import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 export default function App() {
+   const [isDarkMode, setIsDarkMode] = React.useState(false);
+   const handelDarkMode = () => {
+      setIsDarkMode(!isDarkMode);
+   };
+
    return (
       <BrowserRouter>
-         <HeaderPage />
-         <ColorsPanelPage />
-         <Routes>
-            <Route path={`/`} element={<HomePage />} />
-            <Route path={`/about`} element={<AboutPage />} />
-            <Route path={`/contact`} element={<ContactPage />} />
-            <Route path={`/services`} element={<ServicesPage />} />
-            <Route path={`/project`} element={<ProjectPage />} />
-            <Route path={`/game`} element={<GamePage />} />
-         </Routes>
+         <main className={`${isDarkMode ? "dark" : ""}`}>
+            <HeaderPage />
+            <ColorsPanelPage />
+            <Routes>
+               <Route path={`/`} element={<HomePage />} />
+               <Route path={`/about`} element={<AboutPage />} />
+               <Route path={`/contact`} element={<ContactPage />} />
+               <Route path={`/services`} element={<ServicesPage />} />
+               <Route path={`/project`} element={<ProjectPage />} />
+               <Route path={`/game`} element={<GamePage />} />
+            </Routes>
+            <section className="fixed top-20 left-2">
+               <div
+                  className="flex justify-center items-center rounded-full p-4 w-3 h-3 border-2 cursor-pointer border-gray-300 hover:border-gray-400 active:shadow-lg transition-all duration-500 ease-in-out"
+                  onClick={handelDarkMode}
+               >
+                  <div>
+                     {isDarkMode ? <BsFillSunFill className="text-white" /> : <BsMoonStarsFill />}
+                  </div>
+               </div>
+            </section>
+         </main>
       </BrowserRouter>
    );
 }
