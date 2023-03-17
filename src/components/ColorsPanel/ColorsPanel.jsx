5257ef81c83d0e317a6b5259b392db4aec817c77
amankashyap004 <amankashyap004@gmail.com>
2023-03-17 19:47:01 +0530

    fix dark mode feature css

--- src/components/ColorsPanel/ColorsPanel.jsx
+++ src/components/ColorsPanel/ColorsPanel.jsx
@@ -30,17 +30,17 @@ export default function ColorsPanel() {
       <section className="flex justify-center items-center">
          <div
             className={`cursor-pointer mr-2 flex justify-center items-center transition-all
-            ${isColorsPanel ? "translate-x-0" : "translate-x-[800%] sm:translate-x-[830%]"}`}
+            ${isColorsPanel ? "translate-x-0" : "translate-x-[800%] sm:translate-x-[810%]"}`}
             onClick={handelColorsPanel}
          >
             <AiFillSetting className="text-blue-500 hover:text-blue-400 active:text-blue-600 text-3xl z-10 text-center transition-all duration-500 ease-in-out" />
          </div>
          <div
-            className={`bg-gray-400 dark:bg-gray-600 p-4 flex justify-center items-center rounded-bl-md transition-all
+            className={`bg-gray-400 dark:bg-gray-600 p-4 flex justify-center items-center rounded-bl-md transition-all sm:py-3
             ${isColorsPanel ? "translate-x-0" : "translate-x-full"}`}
          >
             <div
-               className="flex justify-center items-center rounded-full p-4 w-3 h-3 border-2 cursor-pointer border-gray-500 dark:border-gray-300 hover:border-gray-600 dark:hover:border-gray-400  active:shadow-lg transition-all duration-500 ease-in-out"
+               className="flex justify-center items-center rounded-full p-4 w-3 h-3 border-2 cursor-pointer border-gray-500 dark:border-gray-300 hover:border-gray-600 dark:hover:border-gray-400 active:shadow-lg transition-all duration-500 ease-in-out"
                onClick={handelDarkMode}
             >
                <div>
