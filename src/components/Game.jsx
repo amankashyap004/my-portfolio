e0941ea55d33840882de3e84b550683c55f6b621
amankashyap004 <amankashyap004@gmail.com>
2023-03-17 18:27:44 +0530

    dark mode feature in Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,7 +11,9 @@ export default function Game() {
    return (
       <section className="flex justify-center items-center flex-col m-4 md:mx-2 sm:mx-0">
          <div className="mt-7">
-            <h2 className="text-white font-bold text-2xl uppercase sm:text-xl">Game</h2>
+            <h2 className="text-gray-900 dark:text-gray-100 font-bold text-2xl uppercase sm:text-xl">
+               Game
+            </h2>
          </div>
          <div className="w-full flex justify-center items-start mt-4 md:flex-wrap md:mt-2 sm:mt-0">
             <GameCard
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -6,10 +6,10 @@ export default function GameCard({ bgImg, heading, description, link }) {
       <Link to={link ? link : "/"}>
          <section className="ease-in-out delay-150 rounded-lg w-[330px] m-4 cursor-pointer relative sm:mx-0 sm:my-2 sm:px-2 sm:w-full">
             <img src={bgImg} className="w-full rounded-lg opacity-75 hover:opacity-50" />
-            <h3 className="absolute top-0 text-white text-base uppercase pt-2 pl-2 font-black z-10 sm:text-sm">
+            <h3 className="absolute top-0 text-gray-900 dark:text-gray-100 text-base uppercase pt-2 pl-2 font-black z-10 sm:text-sm">
                {heading}
             </h3>
-            <p className="absolute bottom-0 text-white text-sm font-extrabold p-2 z-10">
+            <p className="absolute bottom-0 text-gray-900 dark:text-gray-100 text-sm font-extrabold p-2 z-10">
                {description}&nbsp;...
             </p>
          </section>
