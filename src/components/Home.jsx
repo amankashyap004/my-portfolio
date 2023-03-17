258f0c6e369441db745da37dbcf36de2f27e60bf
amankashyap004 <amankashyap004@gmail.com>
2023-03-17 17:53:21 +0530

    dark mode feature in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,11 @@ import { Link } from "react-router-dom";
 export default function Home() {
    return (
       <section className="flex justify-center items-center p-4 w-full mt-8 sm:flex-col sm:mt-4">
-         <section className="mr-4 text-white w-[40%] p-8 md:w-1/2 md:p-4 sm:w-full">
+         <section className="mr-4 text-gray-900 dark:text-gray-100 w-[40%] p-8 md:w-1/2 md:p-4 sm:w-full">
             <h1 className="font-bold text-3xl">
                Hello, my name is <span className="text-blue-500">Aman Kashyap</span>
             </h1>
-            <div className="font-bold text-2xl text-blue-300 my-6">
+            <div className="font-bold text-2xl text-blue-900 dark:text-blue-300 my-6">
                <TypeAnimation
                   sequence={[
                      "I'm an Engineer",
@@ -32,7 +32,7 @@ export default function Home() {
                contact me. Take a look around and let me know if you have any questions or would
                like to work together!{" "}
                <Link to="/about">
-                  <span className="text-blue-500 hover:text-blue-400 hover:underline font-semibold transition-all duration-500 ease-in-out">
+                  <span className="text-blue-700 dark:text-blue-500 hover:text-blue-900 dark:hover:text-blue-400 hover:underline font-semibold transition-all duration-500 ease-in-out">
                      more
                   </span>
                </Link>
