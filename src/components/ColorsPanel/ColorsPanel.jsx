7c04a02921cab30fe1bdaa45ce9aa1a4ce8e07ff
amankashyap004 <amankashyap004@gmail.com>
2023-03-17 19:40:08 +0530

    dark mode feature

--- src/components/ColorsPanel/ColorsPanel.jsx
+++ src/components/ColorsPanel/ColorsPanel.jsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import { AiFillSetting } from "react-icons/ai";
 import ColorContainer from "./ColorContainer";
+import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 export default function ColorsPanel() {
    const [isColorsPanel, setIsColorsPanel] = useState(false);
-
    const handelColorsPanel = () => {
       setIsColorsPanel(!isColorsPanel);
+   };
+
+   const [isDarkMode, setIsDarkMode] = useState(true);
+   const [isDarkClass, setIsDarkClass] = useState(true);
+
+   if (isDarkClass) {
+      document.body.classList.add("dark");
+   }
+
+   const handelDarkMode = () => {
+      setIsDarkClass(false);
+      setIsDarkMode(!isDarkMode);
+      if (isDarkMode) {
+         document.body.classList.remove("dark");
+      } else {
+         document.body.classList.add("dark");
+      }
    };
 
    return (
       <section className="flex justify-center items-center">
          <div
             className={`cursor-pointer mr-2 flex justify-center items-center transition-all
-            ${isColorsPanel ? "translate-x-0" : "translate-x-[680%] sm:translate-x-[700%]"}`}
+            ${isColorsPanel ? "translate-x-0" : "translate-x-[800%] sm:translate-x-[830%]"}`}
             onClick={handelColorsPanel}
          >
             <AiFillSetting className="text-blue-500 hover:text-blue-400 active:text-blue-600 text-3xl z-10 text-center transition-all duration-500 ease-in-out" />
@@ -22,6 +39,14 @@ export default function ColorsPanel() {
             className={`bg-gray-400 dark:bg-gray-600 p-4 flex justify-center items-center rounded-bl-md transition-all
             ${isColorsPanel ? "translate-x-0" : "translate-x-full"}`}
          >
+            <div
+               className="flex justify-center items-center rounded-full p-4 w-3 h-3 border-2 cursor-pointer border-gray-500 dark:border-gray-300 hover:border-gray-600 dark:hover:border-gray-400  active:shadow-lg transition-all duration-500 ease-in-out"
+               onClick={handelDarkMode}
+            >
+               <div>
+                  {isDarkMode ? <BsFillSunFill className="text-white" /> : <BsMoonStarsFill />}
+               </div>
+            </div>
             <ColorContainer />
          </div>
       </section>
