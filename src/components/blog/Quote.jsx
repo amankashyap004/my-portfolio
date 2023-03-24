539f64c8b8b5686b6acf37670ae01f4409b53960
amankashyap004 <amankashyap004@gmail.com>
2023-03-24 18:21:24 +0530

    add some icons in quote page

--- src/components/blog/Quote.jsx
+++ src/components/blog/Quote.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 export default function Quote() {
    return (
@@ -9,18 +11,19 @@ export default function Quote() {
             </h2>
          </div>
          <div className="w-full flex justify-center items-center mt-4">
-            <section className="flex justify-center items-center w-3/4 h-80 bg-slate-800 rounded p-4">
-               <div className="flex justify-center items-center flex-col w-1/2">
+            <section className="flex justify-between items-center w-3/4 h-80 bg-slate-800 rounded py-4 px-6">
+               <MdArrowBackIos className="text-3xl cursor-pointer text-blue-500" />
+               <div className="flex justify-center items-center flex-col w-[70%] h-full relative">
+                  <FaQuoteLeft className="font-extrabold text-3xl text-blue-700 top-0 absolute" />
                   <h3 className="text-center text-xl">
-                     <span className="font-extrabold text-2xl text-blue-500">"</span>When we do the
-                     best we can, we never know what miracle is wrought in our life or the life of
-                     another.
-                     <span className="font-extrabold text-2xl text-blue-500">"</span>
+                     When we do the best we can, we never know what miracle is wrought in our life
+                     or the life of another.
                   </h3>
                   <h2 className="font-bold text-2xl text-right w-full text-gray-300 mt-1">
                      — Helen Keller
                   </h2>
                </div>
+               <MdArrowForwardIos className="text-3xl cursor-pointer text-blue-500" />
             </section>
          </div>
       </main>
