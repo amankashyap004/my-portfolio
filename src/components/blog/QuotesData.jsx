a40ed2789676eac10f3dd92741bb69172d1e032d
amankashyap004 <amankashyap004@gmail.com>
2023-03-28 12:51:59 +0530

    create quotes data

---
+++ src/components/blog/QuotesData.jsx
@@ -0,0 +1,17 @@
+quotes = [
+   "When we do the best we can, we never know what miracle is wrought in our life or the life of another. — Helen Keller",
+
+   "Life is what happens when you're busy making other plans. — John Lennon",
+
+   "You only live once, but if you do it right, once is enough. — Mae West",
+
+   "If you want to live a happy life, tie it to a goal, not to people or things. — Albert Einstein",
+
+   "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking. — Steve Jobs",
+
+   "The big lesson in life, baby, is never be scared of anyone or anything. — Frank Sinatra",
+
+   "Life is not a problem to be solved, but a reality to be experienced. — Soren Kierkegaard",
+
+   "Life is like riding a bicycle. To keep your balance, you must keep moving. — Albert Einstein",
+];
